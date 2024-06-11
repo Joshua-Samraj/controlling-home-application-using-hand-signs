8566cd7eaceebfaea37be755dd1a3d2eb91f31ca
Joshua Samraj <151136911+Joshua-Samraj@users.noreply.github.com>
2024-06-11 18:13:59 +0530

    Update hand_detection_program.py

--- hand_detection_program.py
+++ hand_detection_program.py
@@ -1,9 +1,12 @@
 import cv2
 import os
-from cvzone.HandTrackingModule import HandDetector
+from gpiozero import LED, Servo
 
+from cvzone.HandTrackingModule import HandDetector
+import RPi.GPIO as GPIO
 import pygame
 from time import sleep
+import cvzone
 
 score = 0
 count = 0
@@ -11,9 +14,9 @@ path = os.getcwd()
 score1 = 0
 thicc = 0
 thicc1 = 0
-led1 = LED(18)
-led2 = LED(2)
-led3 = LED(22)
+light1 = LED(18)
+light2 = LED(2)
+light3 = LED(22)
 led4 = LED(23)
 led5 = LED(24)
 
@@ -23,338 +26,146 @@ M1 = LED(17)
 M2 = LED(27)
 
 
-
 cap = cv2.VideoCapture(0)
-
 detector = HandDetector(detectionCon=0.8, maxHands=1)
-#mySerial = cvzone.SerialObject("COM8",9600,1)
-values = range(5)
+
+# Initialize scores and thickness
 
 
+score2 = 0
+thicc2 = 2
 
-
-def play_audio(audio_path_file):
-    #os.system('mpg123 ' + audio_path_file)
-    print(audio_path_file)
-
-
-def led_code_0():
-    print("0")
-    led1.off()
-    led2.off()
-    led3.off()
-   
-def led_code_1():
-    print("1 ON ")
-    audio_file_path = "/home/pi/Desktop/final_with_mp3_and_mp4/opencv_project/final_voices/led_1_on.mp3"
-    play_audio(audio_file_path)
-    led1.on()
-    
-    
-def led_code_1_off():
-    print("1 off ")
-    audio_file_path = "/home/pi/Desktop/final_with_mp3_and_mp4/opencv_project/final_voices/led_1_on.mp3"
-    play_audio(audio_file_path)
-    led1.off()
-       
-
-
-def led_code_2():
-    print("2 ON")
-    
-    
-    led2.on()
-    
-
-    
-    audio_file_path = "/home/pi/Desktop/final_with_mp3_and_mp4/opencv_project/final_voices/led_2_on.mp3"
-    play_audio(audio_file_path)
-    
-
-
-def led_code_2_off():
-    print("2 Off")
-    
-    led2.off()
-
-
-def led_code_3():
-    print("3 ON")
-    
-    led3.on()
-    
-    
-
-    audio_file_path ="/home/pi/Desktop/final_with_mp3_and_mp4/opencv_project/final_voices/led_3_on.mp3"
-    play_audio(audio_file_path)
-
-def led_code_3_off():
-    print("3 off")
-    led3.off()
-
-def code_run():
-    led5.on()
-    if fingerUp == [1,0,0,0,1]:
-        
-
-        audio_file_path = "/home/pi/Desktop/final_with_mp3_and_mp4/opencv_project/final_voices/led_control_started.mp3"
-        play_audio(audio_file_path)
-    elif fingerUp == [0, 1, 0, 0, 0]:
-        cv2.putText(frame, 'Finger count:1', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1,
-                    cv2.LINE_AA)
-        # print("one")
-        for _ in values:
-            led_code_1()
-    elif fingerUp == [0, 1, 1, 0, 0]:
-        cv2.putText(frame, 'Finger count:2', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1,
-                    cv2.LINE_AA)
-
-        for _ in values:
-            led_code_2()
-    elif fingerUp == [0, 1, 1, 1, 0]:
-        cv2.putText(frame, 'Finger count:3', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1,
-                    cv2.LINE_AA)
-        led_code_3()
-
-    elif fingerUp == [0, 0, 0, 0, 1]:
-        cv2.putText(frame, 'Finger count:1', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1,
-                    cv2.LINE_AA)
-        # print("one")
-        for _ in values:
-            led_code_1_off()
-    elif fingerUp == [0, 0, 0, 1, 1]:
-        cv2.putText(frame, 'Finger count:2', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1,
-                    cv2.LINE_AA)
-
-        for _ in values:
-            led_code_2_off()
-    elif fingerUp == [0, 0, 1, 1, 1]:
-        cv2.putText(frame, 'Finger count:3', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1,
-                    cv2.LINE_AA)
-        led_code_3_off()
-#    elif fingerUp == [0, 1, 1, 0, 0]:
-#        cv2.putText(frame, 'low speed', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1,
-#                    (255, 255, 255), 1)
-#        medium_speed()
-#   elif fingerUp == [0, 1, 0, 0, 0]:
-#       cv2.putText(frame, 'fan medium speed', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1,
-#                   (255, 255, 255), 1)
-#       low_speed()
-
-#   elif fingerUp == [0, 1, 1, 1, 0]:
-#       cv2.putText(frame, 'fan high speed', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1,
-#                   (255, 255, 255), 1)
-#       high_speed()
-    elif fingerUp == [0, 0, 1, 0, 0]:
-        vilakamaru()
-
-
-
-
-
-
-
-
-
-
-
-
-def fan_on():
-    print("Fan on")
-    
-def fan_off():
-    print("no action")
-    M1.off()
-    M2.off()
-    
-
-def exit_from_led_control():
-    
-    led5.off()
-
-    print("Thank you")
-    
-
-  
-
-    audio_file_path = "/home/pi/Desktop/final_with_mp3_and_mp4/opencv_project/final_voices/exit_from_led_control.mp3"
-    play_audio( audio_file_path)
-
-
-
-def low_speed():
-    print("fan in low speed")
-    M1.on()
-    M2.off()
-    
-    audio_path = "/home/pi/Desktop/final_with_mp3_and_mp4/opencv_project/final_voices/fan_in_low_speed.mp3"
-    play_audio(audio_path)
-def high_speed():
-    print("Fan in high speed")
-    M1.off()
-    M2.off()
- 
-    audio_path = "/home/pi/Desktop/final_with_mp3_and_mp4/opencv_project/final_voices/fan_in_high_speed.mp3"
-    play_audio( audio_path)
-def medium_speed():
-    print("Fan in medium")
-    
-    M1.off()
-    M2.on()
-    audio_path = "/home/pi/Desktop/final_with_mp3_and_mp4/opencv_project/final_voices/fan_in_medium_speed.mp3"
-    play_audio(audio_path)
-
-
-
-
-
-
-
-
-
-
-
-def fan_run_controls():
-
-    if fingerUp == [1, 1, 0, 0, 0]:
-        
-        cv2.putText(frame, 'fan control started', (20, 320), cv2.FONT_HERSHEY_COMPLEX, 1,
-                    (255, 255, 255), 1)
-        
-        led4.on()
-        
-
-
-        audio_path = "/home/pi/Desktop/final_with_mp3_and_mp4/opencv_project/final_voices/fan_control_stated.mp3"
-        play_audio(audio_path)
-        
-
-    elif fingerUp == [0, 0, 0, 0, 1]:
-        low_speed()
-    elif fingerUp == [0, 1, 0, 0, 0]:
-        low_speed()
-
-    elif fingerUp == [0, 0, 0, 1, 1]:
-        cv2.putText(frame, 'low speed', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1,
-                    (255, 255, 255), 1)
-        medium_speed()
-    elif fingerUp == [0, 1, 1, 0, 0]:
-        cv2.putText(frame, 'low speed', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1,
-                    (255, 255, 255), 1)
-        medium_speed()
-
-
-    elif fingerUp == [0, 0, 1, 1, 1]:
-        cv2.putText(frame, 'fan high speed', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1,
-                    (255, 255, 255), 1)
-        high_speed()
-    elif fingerUp == [0, 1, 1, 1, 0]:
-        cv2.putText(frame, 'fan high speed', (20, 480), cv2.FONT_HERSHEY_COMPLEX, 1,
-                    (255, 255, 255), 1)
-        high_speed()
-    elif fingerUp == [0, 0, 0, 0, 0]:
-        fan_off()
-
-def exit_from_fan_control():
-    led4.off()
-    audio_path = "/home/pi/Desktop/final_with_mp3_and_mp4/opencv_project/final_voices/exit_fom_fan_control.mp3"
-    play_audio(audio_path)
-
+start_valve = 25
+def light_3_off():
+    print("light_3_off")
+    light3.off()
+def light_3_on():
+    print("light_3_on")
+    light3.on()
+def light_2_off():
+    print("light_2_off")
+    light2.off()
+def light_2_on():
+    print("light_2_on")
+    light2.on()
+def light_1_off():
+    print("light_1_off")
+    light1.off()
+def light_1_on():
+    print("light_1_on")
+    light1.on()
 while True:
-
+    # Capture frame-by-frame
     ret, frame = cap.read()
+    if not ret:
+        print("Failed to capture image")
+        break
+
+    # Detect hands
     hands, frame = detector.findHands(frame)
     if hands:
         lmList = hands[0]
         fingerUp = detector.fingersUp(lmList)
 
-        #print(fingerUp)
-        #mySerial.sendData(fingerUp)
-
-
-
-
-
-
-
-
-        if fingerUp == [1, 1, 0, 0, 0]:
-            score1 = score1 + 1
-
+        # Check finger positions and update scores
+        if fingerUp == [0, 1, 1, 0, 0]:
+            score1 += 1
             cv2.putText(frame, "start", (20, 440), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
-        elif fingerUp == [1, 1, 1, 0, 0]:
-            # audio_file_path = "VOICES\\thank you 1.wav"
-            # play_audio(audio_file_path)
-            score1 = score1 - 1
+        elif fingerUp == [0, 0, 0, 1, 1]:
+            score1 -= 1
             cv2.putText(frame, "end", (20, 440), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
-        
+
         if score1 < 0:
-
             score1 = 0
-            print("bye")
-            exit_from_fan_control()
+           
+            light_2_off()
 
-        cv2.putText(frame, 'fan time:  ' + str(score1), (200, 420), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1,
+        cv2.putText(frame, 'Light 2 time: ' + str(score1), (200, 420), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1,
                     cv2.LINE_AA)
 
-        if score1 > 3:
-            score1 = 4
-            
-            # cv2.imwrite(os.path.join(path, 'image.jpg'), frame)
+        if score1 > start_valve:
+            score1 = start_valve+1
             try:
-                fan_run_controls()
+                light_2_on()
+                
             except:
                 pass
-                if thicc < 6:
-                    thicc = thicc + 2
-                else:
-                    thicc = thicc - 2
-                    if thicc < 2:
-                        thicc = 2
-                cv2.rectangle(frame, (0, 0), (1366, 768), (0, 0, 255), thicc)
+            if thicc < 6:
+                thicc += 2
+            else:
+                thicc -= 2
+                if thicc < 2:
+                    thicc = 2
+            cv2.rectangle(frame, (0, 0), (1366, 768), (0, 0, 255), thicc)
 
-
-        if fingerUp == [1,0,0,0,1]:
-            score = score + 1
-
-            cv2.putText(frame,"start",(20, 460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-        elif fingerUp == [1,1,0,0,1]:
-            #audio_file_path = "VOICES\\thank you 1.wav"
-            #play_audio(audio_file_path)
-            score = score - 1
-            cv2.putText(frame,"end",(20, 460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+        if fingerUp == [0, 1, 0, 0, 0]:
+            score += 1
+            cv2.putText(frame, "start", (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+        elif fingerUp == [0, 0, 0, 0, 1]:
+            score -= 1
+            cv2.putText(frame, "end", (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
 
         if score < 0:
             score = 0
-            
             print("bye")
-            exit_from_led_control()
-            
-        cv2.putText(frame,'led time:  '+str(score),(200, 460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+            light_1_off()
 
-        if score > 3:
-            score = 4
-            
-            
-            
-            #cv2.imwrite(os.path.join(path, 'image.jpg'), frame)
+        cv2.putText(frame, 'light 1 time: ' + str(score), (200, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1,
+                    cv2.LINE_AA)
+
+        if score > start_valve:
+            score = start_valve+1
             try:
+                light_1_on()
                 
-                code_run()
-                
-                
-                    
             except:
                 pass
-                if thicc1 < 6:
-                    thicc1 = thicc1 + 2
-                else:
-                    thicc1 = thicc1 - 2
-                    if thicc1 < 2:
-                        thicc1 = 2
-                cv2.rectangle(frame, (0, 0), (1366, 768), (0, 0, 255), thicc1)
+            if thicc1 < 6:
+                thicc1 += 2
+            else:
+                thicc1 -= 2
+                if thicc1 < 2:
+                    thicc1 = 2
+            cv2.rectangle(frame, (0, 0), (1366, 768), (0, 0, 255), thicc1)
+
+        if fingerUp == [0, 1, 1, 1, 0]:
+            score2 += 1
+            cv2.putText(frame, "start", (20, 440), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+        elif fingerUp == [0, 0, 1, 1, 1]:
+            score2 -= 1
+            cv2.putText(frame, "end", (20, 440), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+
+        if score2 < 0:
+            score2 = 0
+           
+            light_3_off()
+
+        cv2.putText(frame, 'Light 3 time: ' + str(score2), (200,390), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1,
+                    cv2.LINE_AA)
+
+        if score2 > start_valve:
+            score2 = start_valve+1
+            try:
+                light_3_on()
+                
+            except:
+                pass
+            if thicc2 < 6:
+                thicc2 += 2
+            else:
+                thicc2 -= 2
+                if thicc2 < 2:
+                    thicc2 = 2
+            cv2.rectangle(frame, (0, 0), (1366, 768), (0, 0, 255), thicc)
 
 
-    frame = cv2.imshow("frame", frame)
-    key = cv2.waitKey(1)
+    # Display the resulting frame
+    cv2.imshow("frame", frame)
 
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture and destroy all windows
+cap.release()
+cv2.destroyAllWindows()
